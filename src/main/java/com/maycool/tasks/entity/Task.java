95f86e3e7c28aae338adbd4e1b950b7625759987
Maycool Vinicius <maycoolvinicius_nh_@hotmail.com>
2026-06-28 10:03:32 -0300

    feat: add enum Priority

--- src/main/java/com/maycool/tasks/entity/Task.java
+++ src/main/java/com/maycool/tasks/entity/Task.java
@@ -1,12 +1,12 @@
 package com.maycool.tasks.entity;
 
+import com.maycool.tasks.enums.Priority;
 import com.maycool.tasks.enums.TaskStatus;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
-import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDateTime;
@@ -23,7 +23,7 @@ public class Task {
         private String description;
         private TaskStatus status;
         private String category;
-        private String priority;
+        private Priority priority;
         @CreatedDate
         private LocalDateTime createdAt;
         private LocalDateTime dueDate;
---
+++ src/main/java/com/maycool/tasks/enums/Priority.java
@@ -0,0 +1,7 @@
+package com.maycool.tasks.enums;
+
+public enum Priority {
+    LOW,
+    MEDIUM,
+    HIGH
+}
