01f7cae52406b5828b40f850389c37fb4f27048c
Maycool Vinicius <maycoolvinicius_nh_@hotmail.com>
2026-06-28 10:16:42 -0300

    feat: add service

---
+++ src/main/java/com/maycool/tasks/service/TaskService.java
@@ -0,0 +1,11 @@
+package com.maycool.tasks.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+
+@RequiredArgsConstructor
+@Service
+public class TaskService {
+
+}
